1aafcf5e3723be1ae8ba818c6c8c7defa2aea4cb
Mateusz Nurczyski <mnurczynski4@gmail.com>
2024-10-19 18:11:45 -0400

    Added a function setting bounds for the memory area, that kernel allocator operates in.

--- src/kernel/entry.c
+++ src/kernel/entry.c
@@ -4,18 +4,19 @@
 
 volatile bool truth = true;
 
-extern char test();
+extern uint32_t test();
+extern void kset(uint32_t base, uint32_t offset);
 
 void start() {
 	uint16_t volatile* screen = (uint16_t*) 0xB8000;
-
+	kset('K', 'J');
 	while (truth) {
 		for (int i = 0; i < 320 * 200; i ++) {
 			volatile uint8_t* chr = &screen[i];
 
 			__asm ("nop");
 
-			chr[0] = test();
+			chr[0] = (char)test();
 			chr[1] = 0b01001001;
 		}
 	}
--- src/kernel/kmalloc.asm
+++ src/kernel/kmalloc.asm
@@ -6,11 +6,49 @@ bits 32
 section .text
 
 global test
+global kset
 
-
-
-test:
-	mov EAX, 'F'
+;Setting the boundaries of the memory area managed by this allocator
+;Taking 2 arguments: uint32_t size,  void* offset
+kset:
+	mov EAX, [ESP+4]
+	mov [size], EAX
+	sub EAX, 1
+	mov [end], EAX
+	mov EAX, [ESP+8]
+	mov [offset], EAX
 	ret
 
 
+
+
+
+
+;Function for test purposes - retrieving things from memory.
+test:
+	mov EAX, [offset]
+	ret
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+section .data
+
+end: dd 0
+size: dd 0
+offset: dd 0
