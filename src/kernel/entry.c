986830b1e57bc1e7815d6549fc46696b1c2ded26
magistermaks <magistermaks@darktree.net>
2024-10-20 14:09:08 +0200

    Limited kprint() function

--- src/kernel/entry.c
+++ src/kernel/entry.c
@@ -1,108 +1,14 @@
 
 #include "types.h"
 #include "console.h"
-#include "ansi.h"
+#include "print.h"
 
 extern char test();
 
 void start() {
 	con_init(80, 25);
 
-	for (int i = 0; i < 7; i ++) {
-		con_write('0');
-		con_write('1');
-		con_write('2');
-		con_write('3');
-	}
-
-	con_write(ANSI_LINE_FEED);
-
-	con_write('1');
-	con_write('2');
-	con_write('3');
-	con_write(ANSI_CARRIAGE_RETURN);
-	con_write('+');
-
-	con_write(ANSI_ESCAPE);
-	con_write(ANSI_CSI);
-	con_write('9');
-	con_write('4');
-	con_write(';');
-	con_write('1');
-	con_write('0');
-	con_write('1');
-	con_write(ANSI_CSI_SGR);
-
-	con_write(ANSI_TAB);
-	con_write('4');
-	con_write('5');
-
-	con_write(ANSI_ESCAPE);
-	con_write(ANSI_CSI);
-	con_write('7');
-	con_write(ANSI_CSI_SGR);
-
-	con_write(ANSI_TAB);
-	con_write('4');
-	con_write('5');
-
-	con_write(ANSI_ESCAPE);
-	con_write(ANSI_CSI);
-	con_write('2');
-	con_write('7');
-	con_write(ANSI_CSI_SGR);
-
-	con_write(ANSI_TAB);
-	con_write('4');
-	con_write('5');
-
-	con_write(ANSI_ESCAPE);
-	con_write(ANSI_CSI);
-	con_write('3');
-	con_write('9');
-	con_write(ANSI_CSI_SGR);
-
-	con_write(ANSI_TAB);
-	con_write('6');
-	con_write('7');
-
-	con_write(ANSI_ESCAPE);
-	con_write(ANSI_CSI);
-	con_write('4');
-	con_write('9');
-	con_write(ANSI_CSI_SGR);
-
-	con_write(ANSI_TAB);
-	con_write('8');
-	con_write('9');
-
-	con_write(ANSI_ESCAPE);
-	con_write(ANSI_CSI);
-	con_write(ANSI_CSI_SGR);
-
-	con_write(ANSI_TAB);
-	con_write('a');
-	con_write('b');
-
-	con_write(ANSI_ESCAPE);
-	con_write(ANSI_CSI);
-	con_write('9');
-	con_write(';');
-	con_write('1');
-	con_write(ANSI_CSI_HVP);
-
-	con_write('X');
-
-
-	con_write(ANSI_ESCAPE);
-	con_write(ANSI_CSI);
-	con_write('0');
-	con_write(ANSI_CSI_EL);
-
-//	con_write(ANSI_ESCAPE);
-//	con_write(ANSI_CSI);
-//	con_write('2');
-//	con_write(ANSI_CSI_SD);
+	kprintf("%% Hello \e[1;33m%s\e[m wo%cld, party like it's \e[1m0x%x\e[m again!", "sweet", 'r', 1920);
 
 	while (true) {
 		__asm("hlt");
--- src/kernel/types.h
+++ src/kernel/types.h
@@ -1,3 +1,4 @@
+#pragma GCC system_header
 
 typedef unsigned char bool;
 typedef unsigned char uint8_t;
@@ -7,3 +8,13 @@ typedef unsigned int size_t;
 
 #define true 1
 #define false 0
+
+// varargs
+#ifndef va_start
+typedef __builtin_va_list va_list;
+
+#	define va_start __builtin_va_start
+#	define va_end __builtin_va_end
+#	define va_copy __builtin_va_copy
+#	define va_arg __builtin_va_arg
+#endif
